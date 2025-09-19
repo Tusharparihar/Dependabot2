020d6298260476a3a3453a7c79a8aeb4a8fc77b8
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-19 12:01:39 +0530

    Create main.tf

---
+++ test-multiple-providers/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.45.0"  # Test if both providers get updated
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {}
+
+resource "azurerm_resource_group" "test" {
+  name     = "rg-dependabot-test-multiple"
+  location = "East US"
+}
+
+#Expected Behavior: Both Azure providers should be updated independently
