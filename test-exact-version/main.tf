066c57c18f1bac805f7bf6eddb610b05979cdea8
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-19 11:56:09 +0530

    Create main.tf

---
+++ test-exact-version/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.80.0"  # Exact version - should Dependabot update this?
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "rg-dependabot-test-exact"
+  location = "East US"
+}
+##Expected Behavior: Dependabot should suggest updates to newer exact versions (e.g., =3.81.0, =3.82.0)
