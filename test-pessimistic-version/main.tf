560db9e12c7dfbeaf2225b3912452580ee7d0b22
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-19 11:56:45 +0530

    Create main.tf

---
+++ test-pessimistic-version/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80.0"  # Allow patch updates only (3.80.x)
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "rg-dependabot-test-pessimistic"
+  location = "East US"
+}
+
+##Expected Behavior: Dependabot should update within patch range (3.80.x → 3.80.y) but not minor (3.81.0)
