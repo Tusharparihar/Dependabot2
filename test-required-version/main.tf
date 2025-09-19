3ab5ceeb94957a04db228a39c7bf2e2510b2adcc
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-19 12:01:08 +0530

    Create main.tf

---
+++ test-required-version/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"  # Should NOT be updated by Dependabot
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"  
+      version = "~> 3.80.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "rg-dependabot-test-required-version"
+  location = "East US"
+}
+#Expected Behavior: Provider version should be updated, but required_version should remain unchanged
