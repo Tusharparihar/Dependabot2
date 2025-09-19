a128c5f892a1ffd090ee6c9c0aeb77acc7b62497
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-19 11:58:35 +0530

    Create main.tf

---
+++ test-range-version/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.80.0, < 4.0.0"  # Range: 3.80.0+ but less than 4.0.0
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "rg-dependabot-test-range"
+  location = "East US"
+}
+
+##Expected Behavior: Dependabot should suggest updates within the 3.x range
