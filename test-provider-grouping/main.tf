4d0298362c3bbaf2fc91c84b3fd524d48ffd3df4
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-19 12:36:24 +0530

    Create main.tf

---
+++ test-provider-grouping/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    # Azure Resource Manager
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80.0"  # Old version to force update
+    }
+    
+    # Azure Active Directory  
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.45.0"  # Old version to force update
+    }
+    
+    # Azure API (newer provider)
+    azapi = {
+      source  = "azure/azapi" 
+      version = "~> 1.9.0"   # Old version to force update
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {}
+
+provider "azapi" {}
+
+# Sample resources to make it realistic
+resource "azurerm_resource_group" "test" {
+  name     = "rg-dependabot-grouping-test"
+  location = "East US"
+}
+
+resource "azuread_group" "test" {
+  display_name = "dependabot-test-group"
+}
